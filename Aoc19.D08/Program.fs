d6fc1c54a3f6b2cc13fd3a045488dfc7887bb3e4
Blaž Peterlin <blasius.peterlin@gmail.com>
2022-07-08 16:02:48 +0200

    Added template D08

---
+++ Aoc19.D08/Program.fs
@@ -0,0 +1,27 @@
+﻿module D08.Solution
+
+open Aoc19
+open Aoc19.Common
+open Aoc19.Input
+open Aoc19.Operators
+
+let env = P
+
+let parse text =
+    let tkns = env |> env2f |> f2tokens "," 
+    let nums = tkns |> List.map int
+    nums
+
+let solve1 text =
+    let input = parse text
+    0
+
+let solve2 text =
+    let input = parse text
+    0
+
+let text = env |> env2f |> f2text
+let res1 = text |> solve1
+let res2 = text |> solve2
+
+let finished = true
---
+++ Aoc19.Test/T08.fs
@@ -0,0 +1,12 @@
+module Aoc19.T08
+
+open NUnit.Framework
+open D08.Solution
+
+[<SetUp>]
+let Setup () =
+    ()
+
+[<Test>]
+let Test1 () =
+    Assert.Pass()
